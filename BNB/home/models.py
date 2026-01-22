7dfe971a2a8c8301a66acba7d83ee6ad540d861d
Jochem Bosch <117586494+J0C0M@users.noreply.github.com>
2026-01-22 01:51:36 +0100

    Frontend couple base pages -added images directory -models.py got working child pages

--- BNB/home/models.py
+++ BNB/home/models.py
@@ -1,11 +1,34 @@
 from django.db import models
-
-from wagtail.models import Page
 from wagtail.fields import RichTextField
+from wagtail.models import Page
 from wagtail.admin.panels import FieldPanel
 
 
 class HomePage(Page):
-    body = RichTextField(blank=True)
+    template = "home/home_page.html"
 
-    content_panels = Page.content_panels + [FieldPanel("body")]
+    subpage_types = ["home.ContentPage", "home.ActivitiesPage"]
+
+
+class ContentPage(Page):
+    template = "home/content_page.html"
+    body = RichTextField(blank=True)
+    intro = RichTextField(blank=True)
+
+    parent_page_types = ['home.HomePage']
+
+    content_panels = Page.content_panels + [
+        FieldPanel("intro", classname="full"),
+        FieldPanel("body", classname="full"),
+    ]
+
+class ActivitiesPage(Page):
+    template = "home/activities_page.html"
+
+    intro = RichTextField(blank=True)
+
+    parent_page_types = ['home.HomePage']
+
+    content_panels = Page.content_panels + [
+        FieldPanel("intro"),
+    ]
